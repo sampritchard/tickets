3cc216f5c4c31ffcf1971a8b7ef15682e4f4a343
Sam Pritchard <sampritchard@msn.com>
2017-08-05 16:09:10 +0100

    Create a multi dimensional array to store the random numbers in. Currently storing 1-400.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tickets
+
+Thought Process:
+- Create an Array of Arrays so it will be a 20*20 grid.
+- Generate random seed data and input into each array.
--- src/Grid.js
+++ src/Grid.js
@@ -1,4 +1,26 @@
 function Grid() {
-  this.rows = 20;
-  this.columns = 20;
+
 }
+
+Grid.prototype.populateGrid = function () {
+  var array = new Array(20)
+
+  for (var i = 0; i < 20; i++)
+    array[i] = new Array(20)
+
+
+  var start = 1;
+
+  for (var i = 0; i < 20; i++) {
+    for (var j = 0; j < 20; j++) {
+      array[i][j] = start;
+      start = start + 1;
+    }
+  }
+
+  for (var i = 0; i < 20; i++) {
+    for (var j = 0; j < 20; j++) {
+    console.log(array[i][j]);
+    }
+  }
+};
