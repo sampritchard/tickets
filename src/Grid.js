787ddc9615793bdb5d6ee1252659c765cfd40bde
Sam Pritchard <sampritchard@msn.com>
2017-08-06 11:55:43 +0100

    Added random event number (upto 400) along with random ticket price

--- src/Grid.js
+++ src/Grid.js
@@ -6,15 +6,9 @@ function Grid() {
   for (var y = 0; y < this.gridWidth; y++) {
     grid.push([]);
     for(var x = 0; x < this.gridHeight; x++) {
-      grid[y].push((Math.random() * 1000).toFixed(2)); // Instead of 0 put random.
+      grid[y].push(("Event " + (Math.random() * 400).toFixed(0))
+                    + (" $" + (Math.random() * 1000).toFixed(2)));
     }
   }
   console.log(grid[19][19])
 }
-
-// function randomPrice() {
-//   var randomNumber = Math.random() * 1000;
-//   var decimals = randomNumber.toFixed(2);
-//   console.log(decimals)
-//   return decimals;
-// }
