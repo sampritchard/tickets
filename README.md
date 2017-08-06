7fcc00b4d7925b6b28ac3a8c3883f6d90059431d
Sam Pritchard <sampritchard@msn.com>
2017-08-06 19:00:24 +0100

    Can enter co-ordinates using a prompt, can show the closest results based on addition only

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Thought Process:
 - Create an Array of Arrays so it will be a 20*20 grid.
 - Print out 400 consecutive numbers on the console and then on the browser
 - find one of those numbers by entering a valid array search. For example (array[0][1]), would return 2.
+- Instead of adding consecutive numbers, change it to a random $ amount.
+- Add random event number(upto 400) to go along side the random $ amount.
+- Return the event next to co-ordinate given.
+- Return the two events either side of the co-ordinate given.
+- Return the two events either side, and the event above or below (three nearest events)
+- Browser asks for co-ordinate input.
+- Returns results in the browser.
--- src/Grid.js
+++ src/Grid.js
@@ -1,14 +1,23 @@
 function Grid() {
   this.gridWidth = 20;
   this.gridHeight = 20;
-  var grid = [];
+  this.grid = [];
 
   for (var y = 0; y < this.gridWidth; y++) {
-    grid.push([]);
+    this.grid.push([]);
     for(var x = 0; x < this.gridHeight; x++) {
-      grid[y].push(("Event " + (Math.random() * 400).toFixed(0))
+      this.grid[y].push(("Event " + (Math.random() * 400).toFixed(0))
                     + (" $" + (Math.random() * 1000).toFixed(2)));
     }
   }
-  console.log(grid[19][19])
 }
+
+Grid.prototype.enterCoordinates = function () {
+  var x = prompt("Please enter the X co-ordinate (Whole Number between 0-19)");
+  var y = prompt("Please enter the Y co-ordinate (Whole Number between 0-19)");
+  console.log(this.grid[parseFloat(x) + 1][parseFloat(y)] + " Distance: 1")
+  console.log(this.grid[parseFloat(x)][parseFloat(y) + 1] + " Distance: 1")
+  console.log(this.grid[parseFloat(x)][parseFloat(y) + 2] + " Distance: 2")
+  console.log(this.grid[parseFloat(x) + 2][parseFloat(y)] + " Distance: 2")
+  console.log(this.grid[parseFloat(x) + 1][parseFloat(y) + 1] + " Distance 2")
+};
